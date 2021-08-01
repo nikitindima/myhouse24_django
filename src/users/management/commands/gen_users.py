2a564390f105a098b7cdfdd24f6b1d1a9dabb91b
Rey092 <roman.cheburan@gmail.com>
2021-08-01 15:24:26 +0300

    gen users fix

--- src/users/management/commands/gen_users.py
+++ src/users/management/commands/gen_users.py
@@ -12,38 +12,40 @@ from src.users.services.user_roles_services import get_or_create_user_roles
 class Command(BaseCommand):
     def handle(self, *args, **options):
         get_or_create_user_roles()
-        fake = Faker()
-        user = User(
-            first_name="Роман",
-            last_name="Чебан",
-            patronymic="Виорелович",
-            email="roman.cheban@ukr.net",
-            status="ACTIVE",
-            user_id=str(random.randint(10000, 99999)),
-            role=get_object_or_404(UserRole, name="Директор"),
-            is_active=True,
-            is_superuser=True,
-            is_staff=True,
-        )
-        user.set_password("admin")
-        user.save()
+        users = User.objects.all()
+        if not users.exists():
+            fake = Faker()
+            user = User(
+                first_name="Роман",
+                last_name="Чебан",
+                patronymic="Виорелович",
+                email="roman.cheban@ukr.net",
+                status="ACTIVE",
+                user_id=str(random.randint(10000, 99999)),
+                role=get_object_or_404(UserRole, name="Директор"),
+                is_active=True,
+                is_superuser=True,
+                is_staff=True,
+            )
+            user.set_password("admin")
+            user.save()
 
-        for _ in range(5):
-            try:
-                user = User(
-                    first_name=fake.first_name(),
-                    last_name=fake.last_name(),
-                    patronymic=fake.last_name(),
-                    email=fake.email(),
-                    status="ACTIVE",
-                    user_id=str(random.randint(10000, 99999)),
-                    role=get_object_or_404(UserRole, name="Управляющий"),
-                    is_active=True,
-                    is_superuser=False,
-                    is_staff=True,
-                )
-                user.set_password("admin")
-                user.save()
+            for _ in range(5):
+                try:
+                    user = User(
+                        first_name=fake.first_name(),
+                        last_name=fake.last_name(),
+                        patronymic=fake.last_name(),
+                        email=fake.email(),
+                        status="ACTIVE",
+                        user_id=str(random.randint(10000, 99999)),
+                        role=get_object_or_404(UserRole, name="Управляющий"),
+                        is_active=True,
+                        is_superuser=False,
+                        is_staff=True,
+                    )
+                    user.set_password("admin")
+                    user.save()
 
-            except IntegrityError:
-                pass
+                except IntegrityError:
+                    pass
